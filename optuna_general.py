20f1e3443d539f8a97f7c640539c5f399d49e7b9
Eddy <ebuss81@googlemail.com>
2025-12-04 10:34:08 +0100

    early stopping cia loss

--- optuna_general.py
+++ optuna_general.py
@@ -44,7 +44,7 @@ def get_args():
     parser.add_argument('--dev_path', type=str, default='Data_raw/2classes/Raw_TS_Classification_dev_17062_samples.pt')
     parser.add_argument('--test_path', type=str, default='Data_raw/2classes/Raw_TS_Classification_test_3412_samples.pt')
     parser.add_argument('--model', type=str, default='Inception1D', choices=['CNN1D', 'Inception1D', 's4', 'mamba'])
-    parser.add_argument('--metric', type=str, default='acc', choices=["acc", "f1_macro"])
+    parser.add_argument('--metric', type=str, default='loss', choices=["acc", "f1_macro", "loss"])
     parser.add_argument('--patience', type=int, default=50, help='Early stopping patience')
 
     # HPO setup
